bb055457db317246cf79018e7675cf3a3e2b8d47
MeetG01 <132537129+MeetG01@users.noreply.github.com>
2023-06-03 15:44:02 +0530

    Update db_sfms.sql

--- DATABASE/db_sfms.sql
+++ DATABASE/db_sfms.sql
@@ -1,28 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jan 31, 2019 at 03:58 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `db_sfms`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `storage`
 --
 
@@ -34,9 +14,7 @@ CREATE TABLE IF NOT EXISTS `storage` (
   `stud_no` int(10) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `storage`
---
+
 
 INSERT INTO `storage` (`store_id`, `filename`, `file_type`, `date_uploaded`, `stud_no`) VALUES
 (3, 'WWW.YTS.AG.jpg', 'image/jpeg', '2019-01-30, 12:27 AM', 14523),
@@ -59,9 +37,7 @@ CREATE TABLE IF NOT EXISTS `student` (
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `student`
---
+
 
 INSERT INTO `student` (`stud_id`, `stud_no`, `firstname`, `lastname`, `gender`, `yr&sec`, `password`) VALUES
 (6, 1353, 'Harry', 'Den', 'Male', '3B', '5f4dcc3b5aa765d61d8327deb882cf99');
@@ -81,9 +57,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user`
---
+
 
 INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `password`, `status`) VALUES
 (1, 'Administrator', '', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'administrator');
